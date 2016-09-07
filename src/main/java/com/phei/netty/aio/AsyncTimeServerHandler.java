33ac554798f8f05659416ef6fde060c6e18e1ffb
luoyong <luoyong_email@163.com>
2016-09-07 16:04:55 +0800

    match book line number

--- src/main/java/com/phei/netty/aio/AsyncTimeServerHandler.java
+++ src/main/java/com/phei/netty/aio/AsyncTimeServerHandler.java
@@ -1,20 +1,4 @@
-/*
- * Copyright 2013-2018 Lilinfeng.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.phei.netty.aio;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.AsynchronousServerSocketChannel;
@@ -27,41 +11,43 @@ import java.util.concurrent.CountDownLatch;
  */
 public class AsyncTimeServerHandler implements Runnable {
 
-	private int port;
+    private int port;
 
-	CountDownLatch latch;
-	AsynchronousServerSocketChannel asynchronousServerSocketChannel;
+    CountDownLatch latch;
+    AsynchronousServerSocketChannel asynchronousServerSocketChannel;
 
-	public AsyncTimeServerHandler(int port) {
-		this.port = port;
-		try {
-			asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open();
-			asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
-			System.out.println("The time server is start in port : " + port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public AsyncTimeServerHandler(int port) {
+	this.port = port;
+	try {
+	    asynchronousServerSocketChannel = AsynchronousServerSocketChannel
+		    .open();
+	    asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
+	    System.out.println("The time server is start in port : " + port);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Runnable#run()
-	 */
-	@Override
-	public void run() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Runnable#run()
+     */
+    @Override
+    public void run() {
 
-		latch = new CountDownLatch(1);
-		doAccept();
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	latch = new CountDownLatch(1);
+	doAccept();
+	try {
+	    latch.await();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	public void doAccept() {
-		asynchronousServerSocketChannel.accept(this, new AcceptCompletionHandler());
-	}
+    public void doAccept() {
+	asynchronousServerSocketChannel.accept(this,
+		new AcceptCompletionHandler());
+    }
 
 }
